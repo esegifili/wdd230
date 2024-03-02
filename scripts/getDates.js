41f881bef97bc83521f0a3855a2e9e46b1827070
esegifili <esegifili@gmail.com>
2024-03-02 00:00:32 -1100

    klb

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,4 +3,5 @@ let currentYear = copyear.getFullYear();
 
 let lastMod = new Date(document.lastModified);
 
+document.getElementById("currentYear").textContent = currentYear;
 document.getElementById("lastMod").innerHTML = "Last modified on : " + lastMod;
