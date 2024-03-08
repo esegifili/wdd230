018a90112a4f5a93657c390952e231e07420eea4
esegifili <esegifili@gmail.com>
2024-03-07 22:54:39 -1100

    ncnx

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,17 +1,5 @@
 const myBtn = document.querySelector('#mode')
 
-const main = document.querySelector('main')
-
 myBtn.addEventListener('click', () => {
-  if (myBtn.textContent.includes('☻')) {
-    main.style.background = '#000'
-    main.style.color = rgba(45, 142, 151, 0.306)
-
-    myBtn.textContent = '☺'
-  } else {
-    main.style.background = 'whitesmoke'
-    main.style.color = 'black'
-
-    myBtn.textContent = '☻'
-  }
+  main.classList.toggle('dark')
 })
