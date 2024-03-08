9205b04101c07f2e3a85cd1053bf19dc63fe1153
esegifili <esegifili@gmail.com>
2024-03-07 17:08:20 -1100

    zgz

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,6 +4,21 @@ const list = documentquerySelector('#list')
 
 button.addEventListener('click', () => {
   if (input.value != '') {
-    alert('Enter Chapter and Verse')
+    input.value = ''
   }
+
+  const li = document.createElement('li')
+  const deleteButton = document.createElement('button')
+
+  li.textContent = input.value
+  deleteButton.textContent = '❌'
+  li.append(deleteButton)
+  list.append(li)
+
+  deleteButton.addEventListener('click', () => {
+    list.removeChild(li)
+  })
+
+  input.focus()
+  input.value = ''
 })
