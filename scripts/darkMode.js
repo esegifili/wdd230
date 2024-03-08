ad42cd0c8cd2288a6522f936b4fb62d94bda5f75
esegifili <esegifili@gmail.com>
2024-03-07 23:13:25 -1100

    nbvh

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,6 +1,6 @@
 const myBtn = document.querySelector('#mode')
 
 myBtn.addEventListener('click', () => {
-  main.classList.toggle('dark')
+  main.classList.toggle('.dark')
   myBtn.classList.toggle('main')
 })
