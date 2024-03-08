0b2ac954cf4f3d8dca01f529bc32381eb4a91dff
esegifili <esegifili@gmail.com>
2024-03-07 16:37:47 -1100

    yffrjy

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button')
+const input = document.querySelector('#favchap')
+const list = documentquerySelector('#list')
+
+button.addEventListener('click', () => {
+  if (input.value != '') {
+    alert('Enter Chapter and Verse')
+  }
+})
