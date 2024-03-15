d55e89f1d76e5f0ee2459bd4f35b51899d891dfc
esegifili <esegifili@gmail.com>
2024-03-14 15:34:31 -1100

    final

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -2,3 +2,17 @@ function dark () {
   const darkBtn = document.querySelector('main')
   darkBtn.classList.toggle('dark-mode')
 }
+
+const visitsDisplay = document.querySelector('.visits')
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0
+
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits
+} else {
+  visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`
+}
+
+numVisits++
+
+localStorage.setItem('numVisits-ls', numVisits)
