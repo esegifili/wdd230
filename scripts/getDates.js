66d5e1a0c50c3ccc9ec0cda02257ae4d374f897c
esegifili <esegifili@gmail.com>
2024-04-29 12:11:40 -1100

    course page commit

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+let copyear = new Date()
+let currentYear = copyear.getFullYear()
+
+let lastMod = new Date(document.lastModified)
+
+document.getElementById('currentYear').textContent = currentYear
+document.getElementById('lastMod').innerHTML = 'Last modified on : ' + lastMod
