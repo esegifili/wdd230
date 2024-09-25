07a22282885ffd2f9559b818c296fea5e20e5078
esegifili <esegifili@gmail.com>
2024-09-25 03:59:46 -1100

    course page enhancements commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,18 @@ document.querySelector('.currentYear').innerHTML = currYear
 const currDate = new Date(document.lastModified)
 document.getElementById('lastModified').innerHTML =
   'Last Modified:' + ' ' + currDate
+
+const burgerBtn = document.querySelector('.menuBtn')
+const navLinks = document.querySelector('.menuLinks')
+
+burgerBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+  burgerBtn.classList.toggle('open')
+})
+
+const darkButton = document.querySelector('.darkBtn')
+const main = document.querySelector('main')
+
+darkButton.addEventListener('click', () => {
+  main.classList.toggle('dark')
+})
