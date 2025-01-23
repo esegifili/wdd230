85b147f343ab23ac52c37ff59e1f565e74c78aef
esegifili <esegifili@gmail.com>
2025-01-23 11:11:24 -1100

    hamburger menu commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,11 @@ document.querySelector('.currYear').innerHTML = currYear
 
 const dateMod = new Date(document.lastModified)
 document.getElementById('lastModified').innerHTML = dateMod
+
+const menuBtn = document.querySelector('#menu')
+const navigation = document.querySelector('.navigation')
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.toggle('show')
+  menuBtn.classList.toggle('show')
+})
