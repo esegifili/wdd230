02e5c219ffd95eaf80e40c248595baaad6f9e69a
esegifili <esegifili@gmail.com>
2025-02-08 04:46:51 -1100

    dark mode commit

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -8,3 +8,16 @@ menuBtn.addEventListener('click', () => {
   navigation.classList.toggle('show')
   menuBtn.classList.toggle('show')
 })
+
+const sun = document.querySelector('#mode')
+const main = document.querySelector('main')
+
+sun.addEventListener('click', () => {
+  if (sun.textContent.includes('☀')) {
+    main.style.background = '#dae0dd'
+    sun.textContent = '☽'
+  } else {
+    main.style.background = '#13181b'
+    sun.textContent = '☀'
+  }
+})
